b4379333cdeaea7e6c50b152e3a5f08acbdb3a44
LEELA-VRAJANATH\Leela <naresh.yakkati@gmail.com>
2024-03-15 23:04:11 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+adjective1 = input("Enter an adjective: ")
+relative = input("Enter a relative: ")
+animal1 = input("Enter an animal: ")
+verb1 = input("Enter a verb (ing form): ")
+place = input("Enter a place: ")
+adjective2 = input("Enter another adjective: ")
+animal2 = input("Enter another animal: ")
+verb2 = input("Enter another verb (ing form): ")
+body_part = input("Enter a body part: ")
+adjective3 = input("Enter a final adjective: ")
+
+story = f"Last night I went to the {adjective1} zoo with my {relative}. We saw a funny {animal1} {verb1} in the {place}.  Then we watched a {adjective2} {animal2} {verb2} with its {body_part}.  We had a {adjective3} time!"
+
+print(story)
